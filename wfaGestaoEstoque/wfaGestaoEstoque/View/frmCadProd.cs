df483aed13b0a48eb3ea72e9b4bcfaf7da95acee
RandomGirl0017 <juliana.carol.amorim@gmail.com>
2019-10-09 16:18:20 -0300

    Commit 1.2: Pequenas inserções

---
+++ wfaGestaoEstoque/wfaGestaoEstoque/View/frmCadProd.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace wfaGestaoEstoque.View
+{
+    public partial class frmCadProd : Form
+    {
+        public frmCadProd()
+        {
+            InitializeComponent();
+        }
+    }
+}
